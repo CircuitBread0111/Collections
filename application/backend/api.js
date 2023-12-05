4b4f2c622819afa5dfa9bc67d4dded1d47a3c54c
Will Brown <wcbrow02@gmail.com>
2023-12-05 17:33:43 -0500

    [backend/api.js] Fix POST and DELETE response codes

--- application/backend/api.js
+++ application/backend/api.js
@@ -17,15 +17,14 @@ exports.getItems = (req, res) => {
 exports.putItem = (req, res) => {
 	if (req.body) {	
 		items.push(req.body);
-		res.status(200).send();
+		res.status(201).send();
 	} else {
 		res.status(400).send("No item object was sent.");
 	}
 };
 
 exports.deleteItem = (req, res) => {
-	console.log(req.params)
 	const { id } = req.params;
 	items.splice(id, 1);
-	res.status(200).send();
+	res.status(204).send();
 }
